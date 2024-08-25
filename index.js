e9a6115ee93fd8e42adafdcf5f965cb569caf0a3
Hirok Sengupta <senguptahirok@gmail.com>
2024-08-25 18:39:13 +0530

    test

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
-
+//test      
 app.use('/public', express.static(`${process.cwd()}/public`));
 
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
+// Your first API endpoint 
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });

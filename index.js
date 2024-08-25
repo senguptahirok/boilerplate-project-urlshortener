4421b603ec9fb9b90e343d0cb3d171c36ae96bd8
Hirok Sengupta <senguptahirok@gmail.com>
2024-08-25 18:43:39 +0530

    reverting changes

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
-//test      
+      
 app.use('/public', express.static(`${process.cwd()}/public`));
 
 app.get('/', function(req, res) {

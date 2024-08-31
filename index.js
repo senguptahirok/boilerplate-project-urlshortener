df88dffd3b4dd48b9ec00536433de05cb9c1f494
Hirok Sengupta <senguptahirok@gmail.com>
2024-09-01 00:52:10 +0530

    starting url shortener

--- index.js
+++ index.js
@@ -14,6 +14,10 @@ app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/shorturl',function(req,res){
+  res.json({'ip': req.ip});
+});
+
 // Your first API endpoint 
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });

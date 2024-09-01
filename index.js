faa55c78a849dce01eb9cfd3da679bba2964eb64
Hirok Sengupta <senguptahirok@gmail.com>
2024-09-01 22:04:10 +0530

    shortURL creation, dns lookup logic

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const URL = require('url').URL;
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -15,7 +16,22 @@ app.get('/', function(req, res) {
 });
 
 app.post('/api/shorturl',function(req,res){
-  res.json({'ip': req.ip});
+ let originalURL = req.body.url;
+ let urlObj = new URL(originalURL);
+ 
+ dns.lookup(urlObj.hostname, function(err, address, family){
+   if (err){
+     res.json({'original_url':originalURL, 'short_url': "invalid URL"});
+   }
+   else{
+     let shortURL = Math.floor(Math.random() * 100000).toString();
+     let data = new Model({'original_url': originalURL, 'short_url': shortURL});
+     data.save(function(err,data){
+      if (err) return console.error(err);
+     });
+     res.json({'original_url': originalURL, 'short_url': shortURL});
+   }
+ }); 
 });
 
 // Your first API endpoint 
